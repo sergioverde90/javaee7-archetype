82c93e60566c26d3a9186e3e94db62b53a7fde73
sergio <sergioverde90@gmail.com>
2016-12-29 20:49:07 +0100

    javaee 7 basic archetype

---
+++ src/main/resources/archetype-resources/src/main/java/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package $com.sergio.archetypes.javaee7_basic_archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class JAXRSConfiguration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
